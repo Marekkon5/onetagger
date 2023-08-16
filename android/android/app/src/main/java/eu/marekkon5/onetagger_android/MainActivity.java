b9020442f296daef365b4605b2865cbcca99cc5d
Marekkon5 <marekkon5@yahoo.com>
2023-08-16 20:34:18 +0200

    Move Android version to its own repo

--- android/android/app/src/main/java/eu/marekkon5/onetagger_android/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package eu.marekkon5.onetagger_android;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- android/onetagger-android/src/lib.rs
+++
@@ -1,4 +0,0 @@
-mod bridge_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
-#[macro_use] extern crate log;
-
-mod api;
