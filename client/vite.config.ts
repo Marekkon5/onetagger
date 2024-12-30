216d0d444684db0cfac72d8609c57025f018a074
Profunktional <iam@yroycedamien.com>
2024-12-29 22:48:41 -0800

    Update dependencies and refine SCSS imports

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,17 +2,18 @@ import { defineConfig } from 'vite';
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue({
             template: { transformAssetUrls }
         }),
-        quasar({
-            sassVariables: 'src/style/quasar.scss'
-        })
+        quasar()
     ],
+    css: {
+        preprocessorOptions: {
+            scss: {}
+        }
+    },
     server: {
         port: 8080
     },
